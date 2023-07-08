97778e91c10ca8fd377e54d0d819bdbf7ea8e738
azizunnabi <aziz484u@gmail.com>
2023-07-08 02:38:20 -0700

    dd
    sss

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-module.exports.connection = async () => {
-  try {
-    await mongoose.connect(process.env.MongoDB);
-    console.log("Database connected");
-  } catch (error) {
-    console.log("Erorr => ", error.message);
-  }
-};
